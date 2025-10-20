81b02d673855c4ba85ad5505a0b7d24ec286c26b
Francisco García <72352101+FranciscoGarcB@users.noreply.github.com>
2025-10-19 21:05:51 -0600

    updated loading_snowflake function

--- source/loading_snowflake.py
+++ source/loading_snowflake.py
@@ -27,7 +27,7 @@ def get_connection():
 
 def load_to_snowflake(conn, dataframe: pd.DataFrame) -> None:
     """
-    Inserts a pandas DataFrame to the Postgresql dabatabse en la tabla raw_weather
+    Inserts a pandas DataFrame to the Snowflake dabatabse 
     """
 
     if dataframe.empty:
@@ -40,45 +40,44 @@ def load_to_snowflake(conn, dataframe: pd.DataFrame) -> None:
             CREATE TEMPORARY TABLE {temp_table} AS SELECT * FROM RAW_WEATHER WHERE 1=0;
         """)
     
-    insert_query = f"""
-        INSERT INTO {temp_table} (CITY_ID, DATE_TIME, TEMPERATURE_CELSIUS, HUMIDITY, PRECIPITATION, WINDSPEED)
-        VALUES (%s, %s, %s, %s, %s, %s)
-    """
+        insert_query = f"""
+            INSERT INTO {temp_table} (CITY_ID, DATE_TIME, TEMPERATURE_CELSIUS, HUMIDITY, PRECIPITATION, WINDSPEED)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """
 
-    data = [
-            (
-                int(r.city_id),
-                r.datetime,
-                float(r.temperature_celsius),
-                float(r.humidity),
-                float(r.precipitation),
-                float(r.windspeed)
-            )
-            for r in dataframe.itertuples(index=False)
-        ]
+        data = [
+                (
+                    int(r.city_id),
+                    r.datetime,
+                    float(r.temperature_celsius),
+                    float(r.humidity),
+                    float(r.precipitation),
+                    float(r.windspeed)
+                )
+                for r in dataframe.itertuples(index=False)
+            ]
+        
+        cur.executemany(insert_query, data)
+        print(f"Inserted {len(data)} rows into {temp_table}")
 
-    cur.executemany(insert_query, data)
-    print(f"Inserted {len(data)} rows into {temp_table}")
-
-    # Massive merge
-    merge_query = f"""
-        MERGE INTO RAW_WEATHER AS target
-        USING {temp_table} AS src
-        ON target.CITY_ID = src.CITY_ID AND target.DATE_TIME = src.DATE_TIME
-        WHEN NOT MATCHED THEN INSERT 
-            (CITY_ID, DATE_TIME, TEMPERATURE_CELSIUS, HUMIDITY, PRECIPITATION, WINDSPEED)
-        VALUES 
-            (src.CITY_ID, src.DATE_TIME, src.TEMPERATURE_CELSIUS, src.HUMIDITY, src.PRECIPITATION, src.WINDSPEED);
-    """
-    cur.execute(merge_query)
-    conn.commit()
-    print("Merge completed successfully.")
-
-    conn.close()
+        # Massive merge
+        merge_query = f"""
+            MERGE INTO RAW_WEATHER AS target
+            USING {temp_table} AS src
+            ON target.CITY_ID = src.CITY_ID AND target.DATE_TIME = src.DATE_TIME
+            WHEN NOT MATCHED THEN INSERT 
+                (CITY_ID, DATE_TIME, TEMPERATURE_CELSIUS, HUMIDITY, PRECIPITATION, WINDSPEED)
+            VALUES 
+                (src.CITY_ID, src.DATE_TIME, src.TEMPERATURE_CELSIUS, src.HUMIDITY, src.PRECIPITATION, src.WINDSPEED);
+            """
+        cur.execute(merge_query)
+        conn.commit()
+        print("Merge completed successfully.")
 
 if __name__ == "__main__":
     start_date = datetime.now() - timedelta(days=3)
     start_date = start_date.strftime("%Y-%m-%d")
+    start_date = "2025-01-01"
     end_date = datetime.now() - timedelta(days=1)
     end_date = end_date.strftime("%Y-%m-%d")
 
@@ -88,15 +87,14 @@ if __name__ == "__main__":
     query = "SELECT CITY_ID, LATITUDE, LONGITUDE FROM cities;"
     cities = pd.read_sql_query(query, conn)
     
-    df_final = pd.DataFrame()
     for index, row in cities.iterrows():
         id = int(row["CITY_ID"])
         lat = row["LATITUDE"]
         lon = row["LONGITUDE"]
 
         temp_df = get_hourly_weather(id, lat, lon, start_date, end_date)
-        df_final = pd.concat([df_final, temp_df])
 
+        load_to_snowflake(conn=conn, dataframe=temp_df)
         print(f"{id}/{len(cities)} cities updated.")
 
-    load_to_snowflake(conn=conn, dataframe=df_final)
+    conn.close()
